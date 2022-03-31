e47b2da66f242475c1b5ce5dbd529124c9f365e5
Evalee <dove_eva@uuuu>
2022-03-31 11:43:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+Vue.createApp({
+    data() {
+        return {
+            plans: [],
+            value: ''
+        }
+    },
+    methods: {
+        addPlan() {
+            this.plans.push(this.value)
+            this.value = ''
+        }
+    }
+}).mount('#myapp')
